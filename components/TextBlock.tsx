4b13915ad4f8722b60d644acf64fe3486044a35a
tomcoolnl2 <tomcoolnl@gmail.com>
2024-01-26 20:35:21 +0100

    refactor: disable nextjs cache when developing locally

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -5,7 +5,7 @@ import { RenderComponentItem } from './hoc/RenderComponent';
 export interface TextBlock {
 	__typename: 'TextBlock';
 	description: any;
-	title: string;
+	title: string | null;
 }
 
 interface Props {
@@ -13,7 +13,7 @@ interface Props {
 }
 
 export const TextBlockComponent: React.FC<Props> = ({ item }) => {
-	const { title, description } = item as TextBlock;
+	const { title = null, description } = item as TextBlock;
 	return (
 		<div className="text-block w-full border-solid border-[#DFDFDF] border-b pb-[20px] mb-[30px] font-montserrat">
 			{title && <h3>{title}</h3>}
