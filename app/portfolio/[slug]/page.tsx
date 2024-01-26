abd01f2de75be2871776fe3c26363009a567542c
tomcoolnl2 <tomcoolnl@gmail.com>
2024-01-26 22:41:11 +0100

    feat: get get detail page and metadata by slug

--- app/portfolio/[slug]/page.tsx
+++ app/portfolio/[slug]/page.tsx
@@ -1,24 +1,75 @@
 import { Metadata, ResolvingMetadata } from 'next';
 import { fetchContentfulData } from '@/lib/api';
-import { parseSeoMetaDataQuery } from '@/lib/utils';
+import { ensureLeadingSlash, parseSeoMetaDataQuery } from '@/lib/utils';
+import { SectionContainer } from '@/components/page/SectionContainer';
 
 type Props = {
 	params: { slug: string };
-	searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-	const pageSeoQuery = parseSeoMetaDataQuery('70FCDbDpk8iLUWWHAU76Ge');
-	const { seoMetaData } = await fetchContentfulData(pageSeoQuery);
+const metaDataQuery = (slug: string): string => `
+	query GetMetaDataBySlug {
+		detailPageCollection(where: { slug: "${slug}" }, limit: 1) {
+			items {
+				seoMetaData {
+          			title
+					description
+					keywords
+				}
+			}
+		}
+	}
+`;
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	//
+	const slug = ensureLeadingSlash(params.slug);
+	const query = metaDataQuery(slug);
+	const {
+		detailPageCollection: {
+			items: [{ seoMetaData }],
+		},
+	} = await fetchContentfulData(query);
 
 	return {
 		...seoMetaData,
 		alternates: {
-			canonical: `https://wiebecool.nl/werk/${params.slug}`,
+			canonical: `https://wiebecool.nl/werk${slug}`,
 		},
 	};
 }
 
+const pageQuery = (slug: string): string => `
+	query GetPageBySlug {
+		detailPageCollection(where: {slug: "${slug}"}, limit: 1) {
+		  items {
+			name
+			title
+			description
+			status
+			creationDate
+			imagesCollection {
+			  items {
+				url
+				description
+			  }
+			}
+		  }
+		}
+	  }
+`;
+
 export default async function DetailPage({ params }: Props) {
-	return <div>My Post: {params.slug}</div>;
+	//
+	const slug = ensureLeadingSlash(params.slug);
+	const query = pageQuery(slug);
+	const {
+		detailPageCollection: {
+			items: [detailPage],
+		},
+	} = await fetchContentfulData(query);
+
+	console.log(params.slug, detailPage);
+
+	return <SectionContainer name={'detail'}>My Post: {params.slug}</SectionContainer>;
 }
