7cb11ad52225d1e69b7ce94e7f421dd84901a342
tomcoolnl2 <tomcoolnl@gmail.com>
2024-01-29 17:18:54 +0100

    fix: moved address query into separate file and validate component

--- components/contact/GoogleMaps.tsx
+++ components/contact/GoogleMaps.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react';
 import { fetchContentfulData, generateGoogleMapsAddress } from '@/lib';
-
-const addressQuery = `
-	query Address {
-		address(id: "VYrkgFK6dR1V81lIJqez2") {
-			postalAddress
-		}
-	}`;
+import AddressQuery from '@/graphql/Address.gql';
 
 export const GoogleMaps = async () => {
-	const { address } = await fetchContentfulData(addressQuery);
+	const { address } = await fetchContentfulData(AddressQuery, { sysID: 'VYrkgFK6dR1V81lIJqez2' });
+	console.log(address);
 	return (
-		<div className="map_wrap w-full float-left clear-both h-auto mb-[50px]">
-			<div className="mapouter">
-				<div className="gmap_canvas">
-					<iframe
-						width="100%"
-						height={355}
-						id="gmap_canvas"
-						src={`https://maps.google.com/maps?q=${generateGoogleMapsAddress(
-							address.postalAddress
-						)},+Netherlands&t=&z=13&ie=UTF8&iwloc=&output=embed`}
-					/>
+		address && (
+			<div className="map_wrap w-full float-left clear-both h-auto mb-[50px]">
+				<div className="mapouter">
+					<div className="gmap_canvas">
+						<iframe
+							width="100%"
+							height={355}
+							id="gmap_canvas"
+							src={`https://maps.google.com/maps?q=${generateGoogleMapsAddress(
+								address.postalAddress
+							)},+Netherlands&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+		)
 	);
 };
