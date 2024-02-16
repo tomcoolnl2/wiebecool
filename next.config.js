f94696184106726bece88837299a35456a26bf53
tomcoolnl2 <tomcoolnl@gmail.com>
2024-02-16 16:16:15 +0100

    chore: installed and ran analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** /** @type {import('next').NextConfig} */
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
@@ -85,4 +89,4 @@ const nextConfig = {
 	},
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
