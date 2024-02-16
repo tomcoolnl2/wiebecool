00348ee6737bda53133393cceb18341b707fde4e
tomcoolnl2 <tomcoolnl@gmail.com>
2024-02-16 13:23:04 +0100

    test(2664): cookie consent

---
+++ cypress/e2e/cookies.cy.ts
@@ -0,0 +1,21 @@
+describe('CookieBar', () => {
+	beforeEach(() => {
+		cy.visit('/');
+	});
+
+	it('should display the cookie consent bar initially', () => {
+		cy.get('.cookie-bar').should('be.visible');
+	});
+
+	it('should hide the cookie consent bar after accepting cookies', () => {
+		cy.get('.cookie-bar').should('be.visible');
+		cy.contains('Accepteer Cookies').click();
+		cy.get('.cookie-bar').should('not.exist');
+	});
+
+	it('should set a cookie after accepting cookies', () => {
+		cy.getCookie('localConsent').should('not.exist');
+		cy.contains('Accepteer Cookies').click();
+		cy.getCookie('localConsent').should('have.property', 'value', 'true');
+	});
+});
