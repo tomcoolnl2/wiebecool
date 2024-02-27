e1ab763eb0f58c943ba9fcc054b197123ed74ff7
tomcoolnl2 <tomcoolnl@gmail.com>
2024-02-27 22:11:57 +0100

    test: unit test for alert component

--- tests/components/Alert.test.tsx
+++ tests/components/Alert.test.tsx
@@ -9,3 +9,16 @@ describe('Alert Snapshot Test', () => {
 		expect(tree).toMatchSnapshot();
 	});
 });
+
+describe('Alert Component', () => {
+	it('renders with the correct message', () => {
+		const message = 'Test Message';
+		const { getByRole } = render(<Alert type="success" message={message} />);
+		expect(getByRole('alert')).toHaveTextContent(message);
+	});
+
+	it('applies the correct type class', () => {
+		const { container } = render(<Alert type="error" message="Error Message" />);
+		expect(container.firstChild).toHaveClass('alert error');
+	});
+});
